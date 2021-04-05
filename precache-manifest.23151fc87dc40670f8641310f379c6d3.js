0335b989437584b657bc74a82f0a7cd5398f75b2
tranquanghuy0801 <huytran.quang080199@gmail.com>
2021-04-05 21:45:34 +1000

    Updates

---
+++ precache-manifest.23151fc87dc40670f8641310f379c6d3.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9cdb244b7f2c99c5d44e43ef8971630",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "101ae1dcee6d9e44450c",
+    "url": "/portfolio/static/css/2.a4c8f85b.chunk.css"
+  },
+  {
+    "revision": "c175e54f9b0f69d4362a",
+    "url": "/portfolio/static/css/main.f0d9e87a.chunk.css"
+  },
+  {
+    "revision": "101ae1dcee6d9e44450c",
+    "url": "/portfolio/static/js/2.cbd78200.chunk.js"
+  },
+  {
+    "revision": "684eaa4876a62d8c9dd2fb91649be61d",
+    "url": "/portfolio/static/js/2.cbd78200.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c175e54f9b0f69d4362a",
+    "url": "/portfolio/static/js/main.95ea2959.chunk.js"
+  },
+  {
+    "revision": "309a9e55c61ba5bff665",
+    "url": "/portfolio/static/js/runtime-main.b3df5f95.js"
+  },
+  {
+    "revision": "2738ffb68b2f3c19c7554bfb19bf328e",
+    "url": "/portfolio/static/media/bill-splitter.2738ffb6.png"
+  },
+  {
+    "revision": "3e655e5ca2febf0443ca55cd020b1799",
+    "url": "/portfolio/static/media/calendar-gspt.3e655e5c.png"
+  },
+  {
+    "revision": "67af05e12311bac06327b43ca31e166b",
+    "url": "/portfolio/static/media/earth.67af05e1.jpg"
+  },
+  {
+    "revision": "b61f96d47d0a29b0e7e18a273cbea286",
+    "url": "/portfolio/static/media/sample.b61f96d4.png"
+  },
+  {
+    "revision": "2026275f30bd05b029fb83c993e69243",
+    "url": "/portfolio/static/media/tweet-analytics.2026275f.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.5aaad0b8a48bf383d9396f42b72f22c6.js"
+  "/portfolio/precache-manifest.23151fc87dc40670f8641310f379c6d3.js"
 );
 
 self.addEventListener('message', (event) => {
